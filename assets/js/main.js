0ec7c8afc1c3a05a2ac29953b134c7e4edfe3b6d
Nabilah Nur Hazimah <nablahnur54@gmail.com>
2025-08-06 18:30:49 +0700

    revisi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,31 @@
-let menuOpen = document.querySelector('.menu-toggle');
-let menuWrapper = document.querySelector('.menu-wrapper')
+function toggleMenu() {
+  const menu = document.getElementById("mobileMenu");
+  const body = document.body;
 
-menuOpen.addEventListener('click', function (){
-    menuOpen.classList.toggle('bx-x');
-    menuWrapper.classList.toggle('active')
-})
+  menu.classList.toggle("active");
+
+  //toggle menu buka tutup dan pengguliran body
+  if (menu.classList.contains("active")) {
+    body.classList.add("menu-open");
+    body.classList.remove("menu-closed");
+    const menuHeight = menu.scrollHeight;
+    body.style.marginTop = `${menuHeight}px`;
+  } else {
+    body.classList.remove("menu-open");
+    body.classList.add("menu-closed");
+    body.style.marginTop = "0";
+  }
+}
+
+// Auto close menu saat link diklik
+document.querySelectorAll("#mobileMenu a").forEach((link) => {
+  link.addEventListener("click", () => {
+    const menu = document.getElementById("mobileMenu");
+    const body = document.body;
+
+    menu.classList.remove("active");
+    body.classList.remove("menu-open");
+    body.classList.add("menu-closed");
+    body.style.marginTop = "0";
+  });
+});
